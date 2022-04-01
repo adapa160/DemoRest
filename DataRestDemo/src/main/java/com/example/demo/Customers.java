88ec6399b8e4f0daf4220c91da9b5854d2764abb
Adapa Saikrishna <adapa160@gmail.com>
2022-04-01 10:12:16 +0200

    Add files via upload

---
+++ DataRestDemo/src/main/java/com/example/demo/Customers.java
@@ -0,0 +1,31 @@
+package com.example.demo;
+
+public class Customers {
+
+	private int cid;
+	private String cname;
+	private String coursename;
+
+	public int getCid() {
+		return cid;
+	}
+	public void setCid(int cid) {
+		this.cid = cid;
+	}
+	public String getCname() {
+		return cname;
+	}
+	public void setCname(String cname) {
+		this.cname = cname;
+	}
+	public String getCoursename() {
+		return coursename;
+	}
+	public void setCoursename(String coursename) {
+		this.coursename = coursename;
+	}
+	@Override
+	public String toString() {
+		return "Customers [cid=" + cid + ", cname=" + cname + ", coursename=" + coursename + "]";
+	}
+}
